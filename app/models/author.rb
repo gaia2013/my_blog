536353e2c90b335a16241c175330011dcc769ce2
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2019-03-08 17:19:52 +0900

    Add annotate

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: authors
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :string
+#  image_url   :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Author < ApplicationRecord
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  category   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ApplicationRecord
   scope :find_newest_article, -> { order(created_at: :desc).limit(5) }
 end
